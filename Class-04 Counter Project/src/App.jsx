3bf45190bc1328c12b31f996b3af5ebe659ee256
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-10-30 17:08:51 +0530

    add conter project

--- Class-04 Counter Project/src/App.jsx
+++ Class-04 Counter Project/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [num, setNum] = useState(0)
+
+  const increase = () => {
+    setNum(num+1);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='p-20'>
+      <h1 className='text-4xl font-bold'>Click the button to increate the value: <span className='text-red-500 text-8xl'>{num}</span></h1>
+      <button onClick={increase} className='bg-blue-500 rounded-lg px-10 py-2 text-white text-xl font-bold mt-5'>Increase</button>
+    </div>
   )
 }
 

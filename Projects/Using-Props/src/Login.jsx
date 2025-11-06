38305ca37b796e377b22d35becc466ce4e6ddb6a
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-11-06 16:20:14 +0530

    add new class content

--- Projects/Using-Props/src/Login.jsx
+++ Projects/Using-Props/src/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Login = ({ setIsSignIn }) => {
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-xl shadow-lg">

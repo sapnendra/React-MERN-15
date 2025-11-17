1b2a12145d208474ce2ef2457e6cc0d1fc5c3acc
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-11-17 16:41:02 +0530

    add use effect class content

---
+++ Class-14 Context-API/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [first, setFirst] = useState(0);
+  const [changing, setChanging] = useState(false);
+
+  useEffect(() => {
+    console.log("Value Changing...");
+  }, [changing]);
+
+  return (
+    <div className="text-2xl">
+      App {first} <br />
+      <button className="border" onClick={() => setFirst((prev) => prev + 1)}>
+        Increment
+      </button>{" "}
+      <br />
+      <button className="border" onClick={() => setChanging((prev) => !prev)}>
+        Change Value
+      </button>
+    </div>
+  );
+};
+
+export default App;

d512c8c3e3ee63a7422ec63e0ef82ba1df10556c
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-12-04 09:03:25 +0530

    Add new class content

--- Class-16 Data Routing/src/components/Footer.jsx
+++ Class-16 Data Routing/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-white flex items-center justify-center py-10 border-t border-gray-300 fixed z-50 w-full bottom-0">
-      <p>&copy; Copyright sapnendra.com | All rights reserved</p>
+      <p className="text-rose-500">&copy; Copyright sapnendra.com | All rights reserved</p>
     </footer>
   );
 };

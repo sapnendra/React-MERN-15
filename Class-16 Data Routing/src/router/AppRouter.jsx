fe490ee90ff240ca15fe2318ca57d07ad81d2a3e
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2025-12-04 18:08:01 +0530

    Add new class content

--- Class-16 Data Routing/src/router/AppRouter.jsx
+++ Class-16 Data Routing/src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import Contact from "../pages/Contact";
 import AppLayout from "../layout/AppLayout";
 import App from "../App";
 import Product from "../pages/Product";
+import { Loader } from "lucide-react";
 
 const AppRouter = () => {
   const router = createBrowserRouter([
@@ -27,6 +28,11 @@ const AppRouter = () => {
               console.log("Error:", error);
             }
           },
+          hydrateFallbackElement: (
+            <span className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] z-50 text-rose-500 inline-block">
+              <Loader className="h-20 w-20 animate-spin"/>
+            </span>
+          ),
           Component: Product,
         },
         {
